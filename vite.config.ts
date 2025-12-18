80386aece0f1e70b4826522e89275fa76b355c5a
m19e <m19e.dev@gmail.com>
2025-12-18 11:46:32 +0900

    💡 Remove done

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import electron from "vite-plugin-electron/simple";
 
-// TODO activate Tailwind CSS
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
