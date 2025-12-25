7a33005416f3d9c48287c516d886dfbc452033a9
m19e <m19e.dev@gmail.com>
2025-12-25 20:54:09 +0900

    ✨ Add for updating app title

---
+++ src/components/MetaHead.tsx
@@ -0,0 +1,18 @@
+import { useAtomValue } from "jotai";
+import { useEffect } from "react";
+
+import { draftFilepathAtom, isSavedAtom } from "@/store/atoms";
+
+export const MetaHead = () => {
+	const isSaved = useAtomValue(isSavedAtom);
+	const draftPath = useAtomValue(draftFilepathAtom);
+
+	useEffect(() => {
+		const appTitle = `${isSaved ? "" : "*"}${draftPath} - Clara Editor 2`;
+
+		// Update the document title
+		document.title = appTitle;
+	}, [isSaved, draftPath]);
+
+	return null;
+};
