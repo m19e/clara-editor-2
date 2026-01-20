f8c5d4be1c31f64c5f8e6231e47846ec40f62478
m19e <m19e.dev@gmail.com>
2026-01-20 14:31:35 +0900

    💡 Remove done

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ import { MetaHead } from "@/components/MetaHead";
 // TODO >フォント設定
 
 // Done
-// TODO 自動保存(挙動だけ)
 // TODO タイトルのマーカー表示
 function App() {
 	const initialConfig: InitialConfigType = {
